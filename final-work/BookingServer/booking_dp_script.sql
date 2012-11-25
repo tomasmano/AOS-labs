7075e5a600efb2c93d9d2ef2d216574dc33781c3
Tomas Mano <tomasmano@gmail.com>
2012-11-25 18:15:24 +0100

    Add booking_dp sql script

---
+++ final-work/BookingServer/booking_dp_script.sql
@@ -0,0 +1,7 @@
+INSERT INTO flight VALUES (1, 300, '2012/12/20 12:00', 150, 'prague', 'paris');
+INSERT INTO flight VALUES (2, 350, '2012/12/20 12:00', 100, 'prague', 'berlin');
+INSERT INTO flight VALUES (3, 250, '2012/12/20 12:00', 200, 'prague', 'london');
+INSERT INTO flight VALUES (4, 200, '2012/12/20 12:00', 100, 'london', 'paris');
+INSERT INTO flight VALUES (5, 300, '2012/12/20 12:00', 200, 'london', 'berlin');
+INSERT INTO flight VALUES (6, 350, '2012/12/20 16:00', 200, 'london', 'prague');
+
